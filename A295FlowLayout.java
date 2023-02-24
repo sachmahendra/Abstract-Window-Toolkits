740e4a738b1603e6917bc1f3e28846916105985f
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-02-24 22:37:31 +0530

    initial.commit
    IT IS A JAVA AWT CONCEPT USE TO DEVELOPE FLOW ORDER IN JAVA APPLICATION

---
+++ A295FlowLayout.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+import java.awt.event.*;
+
+class MyFrame extends Frame{
+    Button b1,b2,b3,b4,b5,b6;
+
+    public MyFrame(){
+        super("flow layout demo");
+
+        b1=new Button("One");
+        b2=new Button("Two");
+        b3=new Button("Three");
+        b4=new Button("Four");
+        b5=new Button("Five");
+        b6=new Button("Six");
+
+        FlowLayout fl = new FlowLayout();
+        fl.setAlignment(FlowLayout.RIGHT);
+        fl.setHgap(100);
+
+        setLayout(fl);
+
+        add(b1);
+        add(b2);
+        add(b3);
+        add(b4);
+        add(b5);
+        add(b6);
+
+    }
+}
+
+public class A295FlowLayout {
+    public static void main(String[] args) {
+        MyFrame f= new MyFrame();
+        f.setSize(500,500);
+        f.setVisible(true);
+    }
+}
