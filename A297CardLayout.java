374e8d176c057710a0810391064690ce9fdae84b
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-02-25 18:15:44 +0530

    initial.commit
    IT IS A JAVA AWT CONCEPT USE TO DEVELOP CARDLAYOUT APPLICATION

---
+++ A297CardLayout.java
@@ -0,0 +1,79 @@
+import java.awt.*;
+import java.awt.event.*;
+
+class MyFrame extends Frame implements ItemListener{
+    Button b1,b2,b3;
+    TextField t1,t2,t3;
+
+    Panel p1;
+    Panel p2;
+    Panel cp;
+
+    Panel mainp;
+
+    Checkbox c1,c2;
+
+    CardLayout cl;
+
+    public MyFrame(){
+        super(" Cardlayout demo");
+
+        CheckboxGroup cg = new CheckboxGroup();
+
+        c1= new Checkbox("one",true,cg);
+        c2= new Checkbox("two",false,cg);
+
+        c1.addItemListener(this);
+       c2.addItemListener(this);
+
+        b1=new Button("One");
+        b2=new Button("Two");
+        b3=new Button("Three");
+
+        t1=new TextField(10);
+        t2=new TextField(10);
+        t3=new TextField(10);
+
+        cp = new Panel();
+        cp.add(c1);
+        cp.add(c2);
+
+        p1=new Panel();
+        p1.add(b1);
+        p1.add(b2);
+        p1.add(b3);
+        
+        p2=new Panel();
+        p2.add(t1);
+        p2.add(t2);
+        p2.add(t3);
+
+        mainp=new Panel();
+       cl=new CardLayout();
+        mainp.setLayout(cl);
+
+        mainp.add("one",p1);
+        mainp.add("two",p2);
+
+        add(cp,BorderLayout.NORTH);
+        add(mainp,BorderLayout.CENTER);
+
+    }
+
+  public void itemStateChanged(ItemEvent ie)
+    {
+        if(c1.getState())
+            cl.first(mainp);
+            else
+                cl.last(mainp);
+    }
+   
+}
+
+public class A297CardLayout {
+    public static void main(String[] args) {
+        MyFrame f = new MyFrame();
+        f.setSize(500,500);
+        f.setVisible(true);
+    }
+}
