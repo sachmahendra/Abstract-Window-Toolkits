598e4a44e0938f714278e3cebfc09e27d8fa1659
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-02-26 23:21:19 +0530

    initial.commit
    IT IS ALSO USE TO UNDER STAND USE LAMBDA FUNCTION IN JAVA APP DEVELOPMENT

---
+++ A302HandilingEventBYLambdaFun.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class MyFrame extends Frame 
+{
+    TextField tf;
+    Button b;
+    int count =0;
+
+    MyFrame()
+    {
+        super("Event Demo");
+        tf=new TextField("0",20) ;
+        b=new Button("Click");
+
+        setLayout(new FlowLayout());
+
+        add(tf);
+        add(b);
+        b.addActionListener((ActionEvent) ->     //HERE USING LAMBDA FUNCTION
+            
+            {
+                count++;
+                tf.setText(String.valueOf(count));
+            }
+        );
+
+    }
+    
+
+}
+
+public class A302HandilingEventBYLambdaFun {
+    public static void main(String[] args) {
+        MyFrame f= new MyFrame();
+        f.setSize(500,500);
+        f.setVisible(true);
+
+    }
+}
