cdbc7ef152a0842a1abb900c6ba9e32eff03fe0c
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-03-08 21:11:04 +0530

    initial.commit
    IT IS A JAVA EXCEPTION HANDILING CONCEPT USE IN INTERTHREAD COMMUNICATION

---
+++ ABInterThreadCommunication195.java
@@ -0,0 +1,135 @@
+/*
+
+class MyData{
+    int value;
+
+    boolean flag = true;
+   synchronized public void set(int v){
+
+
+        value = v;
+     
+    }
+   synchronized  public int get(){
+        int x;
+        x=0;
+        return x;
+    }
+}
+
+class Producer extends Thread{
+    MyData data;
+    public Producer(MyData d){
+        data = d;
+    }
+    public void run(){
+        int count = 1;
+        while(true){
+            data.set(count);
+            System.out.println("Producer: "+count);
+            count++;
+        }
+    }
+}
+
+class Consumer extends Thread{
+    MyData data;
+    public Consumer(MyData d){
+        data = d;
+    }
+    public void run(){
+        int count = 1;
+        while(true){
+            data.set(count);
+            System.out.println("consumer: "+count);
+            count++;
+        }
+    }
+}
+
+public class ABInterThreadCommunication195 {
+    public static void main(String[] args) {
+        MyData data = new MyData();
+
+        Producer p = new Producer(data);
+        Consumer c = new Consumer(data);
+
+        p.start();
+        c.start();
+        
+    }
+}
+*/
+
+
+
+class MyData{
+    int value;
+
+    boolean flag = true;
+   synchronized public void set(int v){
+     while(flag!=true)
+     try{wait();}catch(Exception e){}
+    
+        value = v;
+        flag = false;
+        notify();
+     
+    }
+   synchronized  public int get(){
+
+    int x=0;
+        
+    while(flag!=false)
+    try{wait();}catch(Exception e){}
+        
+        x=value;
+        flag = true;
+        notify();
+
+        return x;
+    }
+}
+
+class Producer extends Thread{
+    MyData data;
+    public Producer(MyData d){
+        data = d;
+    }
+    public void run(){
+        int count = 1;
+        while(true){
+            data.set(count);
+            System.out.println("Producer: "+count);
+            count++;
+        }
+    }
+}
+
+class Consumer extends Thread{
+    MyData data;
+    public Consumer(MyData d){
+        data = d;
+    }
+    public void run(){
+        int count = 1;
+        while(true){
+            data.set(count);
+            System.out.println("consumer: "+count);
+            count++;
+        }
+    }
+}
+
+public class ABInterThreadCommunication195 {
+    public static void main(String[] args) {
+        MyData data = new MyData();
+
+        Producer p = new Producer(data);
+        Consumer c = new Consumer(data);
+
+        p.start();
+        c.start();
+        
+    }
+}
